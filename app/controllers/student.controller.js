8b9ddfc056094f4ed29fd39a9756ad598c702f06
Vento22L <dionisiorockkivn@gmail.com>
2022-10-18 03:21:39 -0500

    Confirm

--- app/controllers/student.controller.js
+++ app/controllers/student.controller.js
@@ -17,17 +17,43 @@ function getOneStudent(req, res){
 }
 
 function addStudent(req, res){
-    const {stud_name, stud_lastname, stud_address, stud_phone, stud_contact_info, stud_gender} =  req.body
-    console.log(`student : ${stud_name}, ${stud_lastname}, ${stud_address}, ${stud_phone}, ${stud_contact_info}, ${stud_gender}`)
-    dataModels.addStudent({student_name, stud_lastname, stud_address, stud_phone, stud_contact_info, stud_gender}, (data, error) => {
+    const {stud_name, 
+        stud_lastname, 
+        stud_address, 
+        stud_phone, 
+        stud_contact_info, 
+        stud_gender} =  req.body
+    console.log(`student : ${stud_name}, 
+        ${stud_lastname}, 
+        ${stud_address}, 
+        ${stud_phone}, 
+        ${stud_contact_info}, 
+        ${stud_gender}`)
+    dataModels.addStudent({student_name, 
+        stud_lastname, 
+        stud_address, 
+        stud_phone, 
+        stud_contact_info, 
+        stud_gender}, (data, error) => {
         res.json(data)
     })
 }
 
 function editStudent(req, res) {
     const { stud_id } = req.params
-    const { stud_name, stud_lastname, stud_address, stud_phone, stud_contact_info, stud_gender } = req.body
-    dataModels.editStudent({stud_id, stud_name, stud_lastname, stud_address, stud_phone, stud_contact_info, stud_gender }, (data, error) => {
+    const { stud_name, 
+        stud_lastname, 
+        stud_address, 
+        stud_phone, 
+        stud_contact_info, 
+        stud_gender } = req.body
+    dataModels.editStudent({stud_id, 
+        stud_name, 
+        stud_lastname, 
+        stud_address, 
+        stud_phone, 
+        stud_contact_info, 
+        stud_gender }, (data, error) => {
         res.json(data)
     })
 }
--- app/routes/student.route.js
+++ app/routes/student.route.js
@@ -2,10 +2,10 @@ const express = require ('express')
 const route = require('express').Router()
 const {getStudents, getOneStudent, addStudent, editStudent, deleteStudent} = require('../controllers/student.controller')
 
-route.route('/students').get(getStudents)
-route.route('/students/:stud_id').get(getOneStudent)
-route.route('/students').post(addStudent)
-route.route('/students/:stud_id').put(editStudent)
-route.route('/students/:stud_id').delete(deleteStudent)
+route.route('/estudiantes').get(getStudents)
+route.route('/estudiantes/:stud_id').get(getOneStudent)
+route.route('/estudiantes').post(addStudent)
+route.route('/estudiantes/:stud_id').put(editStudent)
+route.route('/estudiantes/:stud_id').delete(deleteStudent)
 
 module.exports = route
